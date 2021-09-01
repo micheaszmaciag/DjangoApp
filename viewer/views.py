a72d1f4a47271fd28d74b19f3e84497495c7ac94
michu <michu_009@o2.pl>
2021-09-01 22:54:56 +0200

    Adding movie update

--- viewer/views.py
+++ viewer/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, FormView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
 
 from viewer.models import Movie
@@ -14,35 +14,32 @@ class MoviesView(ListView):
     model = Movie
 
 
-class MovieCreateView(FormView):
+class MovieCreateView(CreateView):
     template_name = 'form.html'
     form_class = MovieForm
     # adres pobrany z URLs na który zostaniemy przekierowani
     # gdy walidacja się powiedzie (movie_create pochodzi z name!)
     success_url = reverse_lazy('movie_create')
 
-    # co ma sie dziać gdy formularz przeszedł walidacje:
-    def form_valid(self, form):
-        # wywołanie metody form_valid z klasy nadrzędnej (FormView)
-        # będziemy zwracać wynik z niej uzyskany
-        result = super().form_valid(form)
-        # w obiekcie cleaned_data przechowujemy wynik działania
-        # funkcji "czyszczących"
-        cleaned_data = form.cleaned_data
-        # zapisujemy do bazy nowy film:
-        Movie.objects.create(
-            title=cleaned_data['title'],
-            genre=cleaned_data['genre'],
-            rating=cleaned_data['rating'],
-            released=cleaned_data['released'],
-            description=cleaned_data['description'],
-        )
-        # Zwracamy result - patrz komentarz nad form_valid
-        return result
-
     # co ma sie dziać gdy formularz nie przejdzie walidacji:
     def form_invalid(self, form):
         # odkładamy w logach informacje o operacji
         LOGGER.warning('User provided invalid data')
+        # zwracamy wynik działania pierwotnej funkcji form_invalid
+        return super().form_invalid(form)
+
+class MovieUpdateView(UpdateView):
+    template_name = 'form.html'
+    form_class = MovieForm
+    # adres pobrany z URLs na który zostaniemy przekierowani
+    # gdy aktualizacja się powiedzie (index pochodzi z name!)
+    success_url = reverse_lazy('index')
+    # Nazwa encji, z której będziemy aktualizować rekord
+    model = Movie
+
+    # co ma się dziać, gdy formularz nie przejdzie walidacji:
+    def form_invalid(self, form):
+        # odkładamy w logach informacje o operacji
+        LOGGER.warning('User provided invalid data when updating')
         # zwracamy wynik działania pierwotnej funkcji form_invalid
         return super().form_invalid(form)

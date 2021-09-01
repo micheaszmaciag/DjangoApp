09b967a75154bed1bf46b094e575498dabd32e5b
michu <michu_009@o2.pl>
2021-09-01 14:02:14 +0200

    Add class base-view

--- viewer/views.py
+++ viewer/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
+from django.views import View
 from viewer.models import Movie
 # Create your views here.
 
-def movies(request):
-    return render(
-        request, template_name='movies.html',
-        context={
-            'movies': Movie.objects.all()
-        }
-    )
+
+
+class MoviesView(View):
+    def get(self, request):
+        return render(
+            request, template_name='movies.html',
+            context={
+                'movies': Movie.objects.all()
+            }
+        )
 

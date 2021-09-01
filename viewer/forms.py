da30218bf55bb2c46ebf0874e32d1022e26a18ac
michu <michu_009@o2.pl>
2021-09-01 21:50:44 +0200

    Preparing MovieForm based on ModelForm

--- viewer/forms.py
+++ viewer/forms.py
@@ -1,26 +1,31 @@
 from django.forms import (
-    Form, CharField, IntegerField, ModelChoiceField, DateField, Textarea
+    ModelForm, CharField, IntegerField,
 
 )
 
-from viewer.models import Genre
 from viewer.validators import PastMonthField, capitalized_validator
 from django.core.exceptions import ValidationError
+from viewer.models import Movie
 import re
 
-class MovieForm(Form):
-    title = CharField(max_length=128)  # input - max: 128
-    genre = ModelChoiceField(queryset=Genre.objects)  # select -> options (pojedynczy wiersz z Genre)
-    rating = IntegerField(min_value=1, max_value=10)  # input type: number, min= 1, max=10
-    released = PastMonthField()  # input type: date
-    description = CharField(widget=Textarea, required=False)  # nie będzie wymaganym polem
 
+class MovieForm(ModelForm):
+    class Meta: #subklasa opisująca dane z których będzie tworzony formularz
+        #model na podstawie którego tworzymy formularz
+        model = Movie
+        #wykorzystujemy wszystkie pola z modelu
+        fields = '__all__'
+
+    #pola z własnymi walidatorami dodajemy oddzielnie poza META
+    title = CharField(validators=[capitalized_validator])
+    rating = IntegerField(min_value=1, max_value=10)
+    released = PastMonthField()
 
     def clean_descrption(self):
         # pobranie wartości pola description
         initial = self.cleaned_data['descrption']
         # podział tekstu na części "od kropki do kropki" - na zdania
-        sentences = re.sub(r'\s*\.\s*','.', initial).split('.')
+        sentences = re.sub(r'\s*\.\s*', '.', initial).split('.')
         # zmiana na wielką literę pierwszej litery każdego ze zdań,
         # dodanie kropki, powtórzenie operacji dla kojnego zadania
         return '. '.join(sentence.capitalize() for sentence in sentences)
@@ -31,7 +36,7 @@ class MovieForm(Form):
             # oznaczenie pola jako błędne bez komentarza
             self.add_error('genre', '')
             self.add_error('rating', '')
-            #rzucamy ogólny błąd / wyjątek
+            # rzucamy ogólny błąd / wyjątek
             raise ValidationError(
                 'Commedies aren\'t so good to be over 7'
             )

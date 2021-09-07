01169b097f96e27af4e4c248ca77c4e9daea84cb
michu <michu_009@o2.pl>
2021-09-07 19:06:47 +0200

    Adding demo , login_requierd repair

--- hollymovies/urls.py
+++ hollymovies/urls.py
@@ -29,7 +29,6 @@ urlpatterns = [
     path('', MoviesView.as_view(), name='index'),
     path('demo', generate_demo, name='demo'), # <==== zmiana
     path('movie/create', MovieCreateView.as_view(), name='movie_create'),
-    path('demo', generate_demo, name='demo'),
     path('movie/update/<pk>', MovieUpdateView.as_view(), name='movie_update'),
     path('movie/delete/<pk>', MovieDeleteView.as_view(), name='movie_delete'),
 
--- viewer/views.py
+++ viewer/views.py
@@ -1,13 +1,26 @@
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.shortcuts import render
 
 from viewer.models import Movie
 from viewer.forms import MovieForm
 
+import datetime # <=== ZMIANA
+
 from logging import getLogger
 
 LOGGER = getLogger()
-
+from django.contrib.auth.decorators import login_required # <=NOWE
+@login_required # <=NOWE
+def generate_demo(request):
+    our_get = request.GET.get('name', '')
+    return render(
+        request, template_name='demo.html',
+        context={'our_get': our_get,
+                 'list': ['pierwszy', 'drugi', 'trzeci', 'czwarty'],
+                 'nasza_data': datetime.datetime.now() # <=== ZMIANA
+                 }
+        )
 
 class MoviesView(ListView):
     template_name = 'movies.html'
